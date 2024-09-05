23cef381baee35072b5d87a33f7efe35aa742c5d
nicolascolini <you@example.com>
2024-09-05 17:28:48 -0300

    update

--- listarCliente.php
+++ listarCliente.php
@@ -1,26 +1,25 @@
+<?php
 
-   <?php
+require 'Banco.php';
+require 'Cliente.php';
 
-    require 'Banco.php';
-    require 'Cliente.php';
+$banco = new Banco();
+$conexao = $banco->getConexao();
 
-    $banco = new Banco();
-    $conexao = $banco->getConexao();
+$cliente = new Cliente($conexao);
+$stmt = $cliente->read();
+$clientes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $cliente = new Cliente($conexao);
-    $stmt = $cliente->read();
-    $clientes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    ?>
+?>
 
 
 <!DOCTYPE html>
 <html lang="pt">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de clientes</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Lista de clientes</title>
 </head>
 <body>
 
@@ -29,38 +28,37 @@
 <table border="1">
 
 <tr>
-    <th>id</th>
-    <th>Nome</th>
-    <th>Telefone</th>
-    <th>Email</th>
-    <th>CPF</th>
-    <th>Ações</th>
+<th>id</th>
+<th>Nome</th>
+<th>Telefone</th>
+<th>Email</th>
+<th>CPF</th>
+<th>Ações</th>
 </tr>
 
 <?php foreach ($clientes as $cliente){ ?>
 
-    <tr>
+<tr>
 
-    <td> <?php echo $cliente['id'];?> </td>
-    <td> <?php echo $cliente['nome'];?> </td>
-    <td> <?php echo $cliente['telefone'];?> </td>
-    <td> <?php echo $cliente['email'];?> </td>
-    <td> <?php echo $cliente['cpf'];?> </td>
-    
-    <td>
-    <a href="form_atualizaCliente.php?id=<?php echo $cliente['id']; ?>">Editar</a> 
-    <a href="deletaCliente.php?id=<?php echo $cliente['id'];?>">Excluir</a> 
-    
-    </td>
+<td> <?php echo $cliente['id'];?> </td>
+<td> <?php echo $cliente['nome'];?> </td>
+<td> <?php echo $cliente['telefone'];?> </td>
+<td> <?php echo $cliente['email'];?> </td>
+<td> <?php echo $cliente['cpf'];?> </td>
 
-    </tr>
+<td>
+<a href="form_atualizaCliente.php?id=<?php echo $cliente['id']; ?>">Editar</a> 
+<a href="deletaCliente.php?id=<?php echo $cliente['id'];?>">Excluir</a> 
 
-    <?php } ?>
+</td>
+
+</tr>
+
+<?php } ?>
 
 </table>
 
 <a href="form_cadastroCliente.php"> Cadastrar novo cliente</a>
-    
+
 </body>
 </html>
-
